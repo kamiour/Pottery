2395f147c0325850d823bd48b733a2a9443dc6fc
Mykhailo Kazakov <kamiournew@gmail.com>
2019-08-04 03:09:21 +0100

    structure and components created

---
+++ js/main.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+
+  var swiper1bottom = new Swiper('.swiper1-bottom', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: true,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  
+  var swiper1top = new Swiper('.swiper1-top', {
+	  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+	  },
+    autoHeight: false,
+    roundLengths: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop:true,
+    loopedSlides: 5, //looped slides should be the same
+    thumbs: {
+      swiper: swiper1bottom,
+    },
+  });
+
+  var swiper3bottom = new Swiper('.swiper3-controls', {
+    spaceBetween: 0,
+    slidesPerView: 5,
+    loop: false,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  
+  var swiper1top = new Swiper('.swiper3-main', {
+	  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+	  },
+    autoHeight: false,
+    roundLengths: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop:false,
+    thumbs: {
+      swiper: swiper3bottom,
+    },
+  });
+  
+  var swiper2 = new Swiper('.swiper2', {
+	  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+	  },
+	  autoplay: {
+		  delay: 3500,
+    },
+    autoHeight: false,
+    roundLengths: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    centeredSlides: false,
+    breakpointsInverse: true,
+      breakpoints: {
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+      960: {
+        slidesPerView: 3,
+        spaceBetween: 24
+      },
+    }
+  });
+
+  $('.burger-menu').on('click', function(e){
+		e.preventDefault();
+		$('.nav-mobile ul').toggleClass('is-open')
+  });
+
+  $('.profile-menu').on('click', function(e){
+		e.preventDefault();
+		$('.profile-menu ul').toggleClass('is-open')
+  });
+  
+})
